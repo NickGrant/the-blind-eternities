d8f1d18a5981d1e50f714d5258b52a6764163510
Nick <nick@nickgrant.io>
2016-05-18 00:05:19 -0700

    Initial commit with working generate script

---
+++ generate.js
@@ -0,0 +1,84 @@
+/*
+ * Generate Data for Application
+ */
+
+/** DEPENDENCIES **/
+var fs = require("fs"),
+    request = require('request'),
+    async = require('async');
+
+/** HELPER FUNCTIONS **/
+var download = function(uri, filename, callback){
+    request.head(uri, function(err, res, body){
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+    });
+};
+
+/** THE GUTS **/
+
+var cards = [],
+    raw_cards = [],
+    lists = [
+        'http://mtgjson.com/json/HOP.json',
+        'http://mtgjson.com/json/PC2.json'
+    ],
+    cur_list = 0;
+
+async.eachSeries(lists,function(url, callback){
+    console.log('getting '+url);
+    request.get(url,function(err, resp, body){
+        if(!err && resp.statusCode == 200 && body){
+            var json = JSON.parse(body);
+            async.each(json.cards,function(card, callback){
+                if(card.type.indexOf('Plane') != -1 || card.type.indexOf('Phenomenon') != -1){
+                    raw_cards.push(card);
+                }
+                callback();
+            },function(err){
+                if(err){
+                    throw err;
+                }
+                console.log('retrieved '+ url);
+            });
+        }
+        callback();
+    });
+},function(err){
+    if(err){
+        throw err;
+    }
+    /** PARSE RAW CARDS **/
+    async.each(raw_cards,function(card, callback){
+        var c = {
+            name: card.name,
+            multiverseid: card.multiverseid,
+            type: card.type,
+            text: card.text,
+            img: card.multiverseid+'.jpg'
+        }
+        if(card.subtypes){
+            c.subtypes = card.subtypes;
+        }
+        //pull image if not exists
+        fs.exists('data/'+c.img,function(res){
+            if(!res){
+                download("http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid="+c.multiverseid+"&type=card",'data/'+c.img,function(){
+                    console.log('downloaded '+c.img);
+                });
+            }
+        });
+        cards.push(c);
+        callback();
+    },function(err){
+        if(err){
+            throw err;
+        }
+        fs.writeFile('data/cards.json',JSON.stringify(cards), function(err){
+            if(err){
+                throw err;
+            }else{
+                console.log('Finished');
+            }
+        });
+    });
+});
---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('nothing yet');
+console.log('try running "npm run generate" instead');
